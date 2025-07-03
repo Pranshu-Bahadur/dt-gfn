f9513e23a64208fa72261e2b074ec1597dcea46b
verus-carver <pranshubahadur@gmail.com>
2025-07-03 10:11:28 +0530

    fixing trainer

--- src/trainer.py
+++ src/trainer.py
@@ -195,7 +195,7 @@ class Trainer:
 
                 dE = torch.zeros_like(log_pf)  # gain term can be added later
 
-                l_tb = tb_loss(log_pf, log_pb, log_z, torch.tensor(R).unsqueeze(0), prior)
+                l_tb = tb_loss(log_pf, log_pb, log_z, torch.tensor([R]).unsqueeze(0), prior)
                 l_fl = fl_loss(logF, log_pf, log_pb, dE)
                 (l_tb + 0.1 * l_fl).backward()
                 tb_acc += l_tb.item()
