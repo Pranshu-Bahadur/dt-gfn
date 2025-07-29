eb346ba4d46cc14af384069d3080a76bf65ab532
root <pranshubahadur@gmail.com>
2025-07-29 07:05:38 +0000

    fixing tb loss reward prior

--- src/utils.py
+++ src/utils.py
@@ -18,7 +18,7 @@ def tb_loss(log_pf: torch.Tensor, log_pb: torch.Tensor, log_z: torch.Tensor, R: 
     """
     Calculates the Trajectory Balance (TB) loss, batched.
     """
-    loss = (log_z + log_pf.sum(1) - (torch.log(R) + prior + log_pb.sum(1)))**2
+    loss = (log_z + log_pf.sum(1) - (torch.log(R) + log_pb.sum(1)))**2
     return loss.mean()
 
 @torch.jit.script
